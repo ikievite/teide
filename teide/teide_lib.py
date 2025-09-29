1ebc41379757540e6ea4f75767f28a839d820ce6
Oleksandr <ikievite@gmail.com>
2025-09-29 10:45:08 +0300

    add user and ip commands

--- teide/teide_lib.py
+++ teide/teide_lib.py
@@ -5,6 +5,31 @@ def send_show_command(node, cmd: str) -> str:
     return node.send_raw_command(cmd)
 
 
+def add_user(node, user_name: str, group: str, password: str) -> str:
+    cmd = f"/user add group={group} name={user_name} password={password}"
+    result = send_show_command(node, cmd)
+    return result
+
+
+def change_user_password(node, user_name: str, password: str) -> str:
+    cmd = f"/user set {user_name} password={password}"
+    result = send_show_command(node, cmd)
+    return result
+
+
+def create_user_group(node, group_name: str, policies: list) -> str:
+    policies = ",".join(policies)
+    cmd = f"/user group/add name={group_name} policy={policies}"
+    result = send_show_command(node, cmd)
+    return result
+
+
+def show_ip_address(node) -> str:
+    cmd = "/ip/address/print"
+    result = send_show_command(node, cmd)
+    return result
+
+
 def show_version(node) -> str:
     cmd = "/system/routerboard/print"
     result = send_show_command(node, cmd)
